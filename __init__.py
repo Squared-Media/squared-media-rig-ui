f45ec75b27baae0a6f225a1b5fb6e39d2f43ec31
Fxnarji <92873527+Fxnarji@users.noreply.github.com>
2024-12-29 17:39:55 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -10,7 +10,7 @@ bl_info = {
     "name": "Squared Media Rig UI Addon",
     "description": "Adds RIG UI for Supported Rigs",
     "author": "Squared Media, Fxnarji",
-    "version": (0, 1, 2),
+    "version": (0, 1, 3),
     "blender": (4, 3, 2),
     "location": "Npanel > SQMDefaultRig",
     "support": "COMMUNITY",
