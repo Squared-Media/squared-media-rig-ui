b76d837def0b324299ca03822cb5bf8f92fc4783
Fxnarji <fyluft@stud.mediadesign.de>
2025-09-22 03:57:54 +0200

    fixed #102

--- panels/VIEW3D_PT_RigListPanel.py
+++ panels/VIEW3D_PT_RigListPanel.py
@@ -1,6 +1,7 @@
-import bpy
-from msc.utils import get_preferences  # type: ignore
+import bpy  # type: ignore
+from ..msc.utils import get_preferences
 from .. import properties
+import os
 
 
 class VIEW3D_PT_RigListPanel(bpy.types.Panel):
@@ -46,17 +47,22 @@ class VIEW3D_PT_RigListPanel(bpy.types.Panel):
 
         # Name / Create Character Button
 
-        operator = "", "squaredmedia.namecharacter"
+        operator = ""
         preferences = get_preferences(context)
-        usr_path = preferences.usr_lib_path
-        if bpy.data.filepath is usr_path:
-            operator = "squaredmedia.namecharacter"
-        else:
-            operator = "squaredmedia.create_character"
+        usr_path = preferences.usr_file_path
 
         row = layout.row()
         row.scale_y = 2
-        row.operator(operator, text="Name Your Character!", icon="SMALL_CAPS")
+        # TODO: implement the create character functionality!
+        if bpy.data.filepath:
+            current_file_abs = os.path.abspath(bpy.data.filepath)
+            if os.path.commonpath([current_file_abs, usr_path]) == usr_path:
+                operator = "squaredmedia.namecharacter"
+                row.operator(operator, icon="SMALL_CAPS")
+            else:
+                operator = "squaredmedia.create_character"
+        else:
+            operator = "squaredmedia.create_character"
 
         if len(rig_props.rigs) > 0:
             sublayout = sublayout.split(factor=0.85, align=True)
